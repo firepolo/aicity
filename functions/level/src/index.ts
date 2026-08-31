468ac85102cf251d0770da9907265399750d5984
Anthony Pasche <apasche@lab4tech.lan>
2026-08-31 10:48:26 +0200

    try to add function

--- functions/level/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { app } from "@azure/functions";
-
-app.serviceBusTopic("level.generate", {
-})
--- functions/npc/src/index.ts
+++ functions/npc/src/index.ts
@@ -1,4 +1,46 @@
-import { app } from "@azure/functions";
+import { ServiceBusMessage } from "@azure/service-bus";
+import { app, InvocationContext, output } from "@azure/functions";
+import { faker } from "@faker-js/faker";
+import { randomUUID } from "crypto";
+
+for (let i = 0; i < 3; ++i) {
+	console.log({
+		firstname: faker.person.firstName(),
+		lastname: faker.person.lastName(),
+		sex: faker.person.sex(),
+		age: faker.number.int({ min: 20, max: 70 }),
+		job: faker.person.jobTitle(),
+		zodiac: faker.person.zodiacSign(),
+		haircolor: faker.color.human(),
+		eyecolor: faker.color.human()
+	});
+}
+
+const busOutput = output.serviceBusTopic({
+	connection: process.env.SERVICE_BUS_ENDPOINT!,
+	topicName: "game"
+});
 
 app.serviceBusTopic("npc.generate", {
+	connection: process.env.SERVICE_BUS_ENDPOINT!,
+	topicName: "game",
+	subscriptionName: "npc.generator",
+	extraOutputs: [busOutput],
+	handler(message: ServiceBusMessage, context: InvocationContext) {
+		if (!message.applicationProperties || !message.applicationProperties.clientId || !message.applicationProperties.type) return;
+
+		const args = message.applicationProperties!;
+
+		context.extraOutputs.set(busOutput, JSON.stringify({
+			messageId: randomUUID(),
+			contentType: "application/json",
+			subject: "game.event",
+			applicationProperties: {
+				clientId: args.clientId,
+				type: "npc.generated"
+			},
+			body: message.body
+		}));
+	},
+	cardinality: "one"
 });
