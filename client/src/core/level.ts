f06caf830ba2b85051e8a2ee0c67e0881d8dbec6
Anthony Pasche <apasche@lab4tech.lan>
2026-09-07 09:50:44 +0200

    optimise rendering

--- client/src/core/level.ts
+++ client/src/core/level.ts
@@ -4,45 +4,16 @@ import { MessageType } from "@game/shared/network";
 import { colors } from "@game/shared/colors";
 import { Npc } from "@/entities/npc";
 import { Mat4 } from "@/math/mat4";
-import { gl } from "./renderer";
-import { Model, models } from "@/renderer/model";
-import { shaders } from "@/renderer/shader";
+import { models } from "@/renderer/model";
+import { ModelInstance } from "@/renderer/modelInstance";
 import { textures } from "@/renderer/texture";
 
-type CellModel = {
-	index: number,
-	transform: number,
-	texture: number
-};
-
-type Cell = {
-	street: CellModel,
-	north: CellModel,
-	west: CellModel,
-	east: CellModel,
-	south: CellModel
-};
+type Cell = ModelInstance[];
 
 const width = 8;
 const bound = width - 1;
 const grid: Cell[] = new Array<Cell>(width * width);
 const npc: Npc[] = [];
-
-const index2Transform = [
-	Mat4.identity(),
-	Mat4.rotateY(Math.PI * 0.5),
-	Mat4.rotateY(Math.PI),
-	Mat4.rotateY(Math.PI * 1.5)
-];
-
-const index2Model: Array<Model> = [];
-
-const index2Cardinal: { key: keyof Cell, transform: number }[] = [
-	{ key: "north", transform: 1 },
-	{ key: "west", transform: 2 },
-	{ key: "east", transform: 0 },
-	{ key: "south", transform: 3 }
-];
 
 export default {
 	load: async (callback: ProgressCallback): Promise<void> => new Promise((res) => {
@@ -53,6 +24,22 @@ export default {
 				const j = 3 + i * 6;
 				npc.push(new Npc(view.getUint32(j), Object.values(colors.hair)[view.getUint8(j + 4)], Object.values(colors.eye)[view.getUint8(j + 5)]));
 			}
+
+			const index2Transform = [
+				Mat4.identity(),
+				Mat4.rotateY(Math.PI * 0.5),
+				Mat4.rotateY(Math.PI),
+				Mat4.rotateY(Math.PI * 1.5)
+			];
+
+			const cardinal2Index = [ 1, 2, 0, 3 ];
+
+			const index2BuildingName = [
+				"building001",
+				"building002",
+				"building003",
+				"building005"
+			];
 
 			const map: number[] = [
 				0,0,0,0,0,0,0,0,
@@ -65,123 +52,47 @@ export default {
 				0,0,0,0,0,0,0,0
 			];
 
-			for (let y = 0; y < width; ++y) {
-				for (let x = 0; x < width; ++x) {
-					const i = y * width + x;
-					grid[i] = {
-						street: { index: map[i] == 0 ? -1 : 0, transform: -1, texture: 0 },
-						north: { index: -1, transform: -1, texture: 0 },
-						west: { index: -1, transform: -1, texture: 0 },
-						east: { index: -1, transform: -1, texture: 0 },
-						south: { index: -1, transform: -1, texture: 0 }
-					};
-				}
-			}
-
 			for (let y = 1; y < bound; ++y) {
 				for (let x = 1; x < bound; ++x) {
 					const i = y * width + x;
-					const cell = grid[i];
-					if (cell.street.index < 0) continue;
+					if (!map[i]) continue;
 
-					const n = grid[i - width];
-					const w = grid[i - 1];
-					const e = grid[i + 1];
-					const s = grid[i + width];
-					const c = ((n.street.index >= 0 ? 1 : 0) << 3) | ((w.street.index >= 0 ? 1 : 0) << 2) | ((e.street.index >= 0 ? 1 : 0) << 1) | (s.street.index >= 0 ? 1 : 0);
-					if (c == 0b0000) {
-						cell.street.index = -1;
-						continue;
-					}
+					const c = (map[i - width] << 3) | (map[i - 1] << 2) | (map[i + 1] << 1) | map[i + width];
+					if (c == 0b0000) continue;
 
-					if (c == 0b0001) {
-						cell.street.index = 5;
-						cell.street.transform = 2;
-					}
-					else if (c == 0b0010) {
-						cell.street.index = 5;
-						cell.street.transform = 0;
-					}
-					else if (c == 0b0011) {
-						cell.street.index = 7;
-						cell.street.transform = 3;
-					}
-					else if (c == 0b0100) {
-						cell.street.index = 5;
-						cell.street.transform = 1;
-					}
-					else if (c == 0b0101) {
-						cell.street.index = 7;
-						cell.street.transform = 2;
-					}
-					else if (c == 0b0110) {
-						cell.street.index = 6;
-						cell.street.transform = 1;
-					}
-					else if (c == 0b0111) {
-						cell.street.index = 8;
-						cell.street.transform = 3;
-					}
-					else if (c == 0b1000) {
-						cell.street.index = 5;
-						cell.street.transform = 0;
-					}
-					else if (c == 0b1001) {
-						cell.street.index = 6;
-						cell.street.transform = 0;
-					}
-					else if (c == 0b1010) {
-						cell.street.index = 7;
-						cell.street.transform = 0;
-					}
-					else if (c == 0b1011) {
-						cell.street.index = 8;
-						cell.street.transform = 0;
-					}
-					else if (c == 0b1100) {
-						cell.street.index = 7;
-						cell.street.transform = 1;
-					}
-					else if (c == 0b1101) {
-						cell.street.index = 8;
-						cell.street.transform = 2;
-					}
-					else if (c == 0b1110) {
-						cell.street.index = 8;
-						cell.street.transform = 1;
-					}
-					else if (c == 0b1111) {
-						cell.street.index = 9;
-						cell.street.transform = 0;
-					}
+					const translate = Mat4.translate(x * 30, 0.0, y * 30);
+
+					const cell: ModelInstance[] = [];
+
+					if (c == 0b0001) cell.push(new ModelInstance(models["streetc"], textures["streetc"], Mat4.mul(index2Transform[2], translate)));
+					else if (c == 0b0010) cell.push(new ModelInstance(models["streetc"], textures["streetc"], Mat4.mul(index2Transform[0], translate)));
+					else if (c == 0b0011) cell.push(new ModelInstance(models["streetl"], textures["streetl"], Mat4.mul(index2Transform[3], translate)));
+					else if (c == 0b0100) cell.push(new ModelInstance(models["streetc"], textures["streetc"], Mat4.mul(index2Transform[1], translate)));
+					else if (c == 0b0101) cell.push(new ModelInstance(models["streetl"], textures["streetl"], Mat4.mul(index2Transform[2], translate)));
+					else if (c == 0b0110) cell.push(new ModelInstance(models["streeti"], textures["streeti"], Mat4.mul(index2Transform[1], translate)));
+					else if (c == 0b0111) cell.push(new ModelInstance(models["streett"], textures["streett"], Mat4.mul(index2Transform[3], translate)));
+					else if (c == 0b1000) cell.push(new ModelInstance(models["streetc"], textures["streetc"], Mat4.mul(index2Transform[0], translate)));
+					else if (c == 0b1001) cell.push(new ModelInstance(models["streeti"], textures["streeti"], Mat4.mul(index2Transform[0], translate)));
+					else if (c == 0b1010) cell.push(new ModelInstance(models["streetl"], textures["streetl"], Mat4.mul(index2Transform[0], translate)));
+					else if (c == 0b1011) cell.push(new ModelInstance(models["streett"], textures["streett"], Mat4.mul(index2Transform[0], translate)));
+					else if (c == 0b1100) cell.push(new ModelInstance(models["streetl"], textures["streetl"], Mat4.mul(index2Transform[1], translate)));
+					else if (c == 0b1101) cell.push(new ModelInstance(models["streett"], textures["streett"], Mat4.mul(index2Transform[2], translate)));
+					else if (c == 0b1110) cell.push(new ModelInstance(models["streett"], textures["streett"], Mat4.mul(index2Transform[1], translate)));
+					else if (c == 0b1111) cell.push(new ModelInstance(models["streetx"], textures["streetx"], Mat4.mul(index2Transform[0], translate)));
 
 					for (let i = 0; i < 4; ++i) {
 						if (c & (1 << i)) continue;
-		
-						const cardinal = index2Cardinal[i];
-						const model = cell[cardinal.key];
-						//model.index = Math.floor(Math.random() * index2Building.length);
-						model.index = 0;
-						model.transform = cardinal.transform;
+						cell.push(new ModelInstance(models[index2BuildingName[Math.floor(Math.random() * index2BuildingName.length)]], textures["building002"], Mat4.mul(index2Transform[cardinal2Index[i]], translate)));
 					}
+
+					if (x == 1 && y == 1) console.log(cell);
+
+					grid[i] = cell;
 				}
 			}
 
 			res();
 		});
-
-		for (const key of [
-			"building001",
-			"building002",
-			"building003",
-			"building004",
-			"building005",
-			"streetc",
-			"streeti",
-			"streetl",
-			"streett",
-			"streetx"
-		]) index2Model.push(models[key]);
 
 		const buffer = new ArrayBuffer(1);
 		const view = new DataView(buffer);
@@ -195,19 +106,8 @@ export default {
 		for (let y = 0; y < width; ++y) {
 			for (let x = 0; x < width; ++x) {
 				const cell = grid[y * width + x];
-				if (cell.street.index < 0) continue;
-
-				gl.bindTexture(gl.TEXTURE_2D, textures["streeti"]);
-				gl.uniformMatrix4fv(shaders["basic"].uniforms["uModel"], false, Mat4.mul(index2Transform[cell.street.transform], Mat4.translate(x * 32, 0, y * 32)));
-				index2Model[cell.street.index].render()
-
-				for (const cardinal of index2Cardinal) {
-					const model = cell[cardinal.key]
-					if (model.index < 0) continue;
-					gl.bindTexture(gl.TEXTURE_2D, textures["building001"]);
-					gl.uniformMatrix4fv(shaders["basic"].uniforms["uModel"], false, Mat4.mul(index2Transform[model.transform], Mat4.translate(x * 32, 0, y * 32)));
-					index2Model[model.index].render()
-				}
+				if (!cell) continue;
+				for (const model of cell) model.render();
 			}
 		}
 	}
---
+++ client/src/renderer/modelInstance.ts
@@ -0,0 +1,22 @@
+import { Mat4 } from "@/math/mat4";
+import { Model } from "./model";
+import { gl } from "@/core/renderer";
+import { shaders } from "./shader";
+
+export class ModelInstance {
+	private readonly model: Model;
+	private readonly texture: WebGLTexture;
+	private readonly transform: Mat4;
+
+	constructor(model: Model, texture: WebGLTexture, transform: Mat4) {
+		this.model = model;
+		this.texture = texture;
+		this.transform = transform;
+	}
+
+	render(): void {
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+		gl.uniformMatrix4fv(shaders["basic"].uniforms["uModel"], false, this.transform);
+		this.model.render();
+	}
+}
